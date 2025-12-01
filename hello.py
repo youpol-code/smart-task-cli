f428c96cef591a113e9e7758ffb1f0df96765029
macpol <macpol@macpols-MacBook-Air.local>
2025-12-01 11:05:56 +0700

    feat: Add loop and list storage with type hits

--- hello.py
+++ hello.py
@@ -1,6 +1,21 @@
+from typing import List
 def main():
-    user_name=input("โปรดใส่ชื่อคุณ: ")
-    print(f"สวัสดีคุณ {user_name}! ยินดีต้อนรับสู่โลก Python")
+    tasks :List[str] = []
+
+    print("--- Smart Task CLI v1.0 ---")
+
+    while True:
+        new_task :str = input("ป้อนงานที่ต้องทำ (หรือพิมพ์ 'exit' เพื่อจบ):")
+
+        if new_task == "exit":
+            print("บ๊ายบาย! จบการทำงาน")
+            break
+        
+        tasks.append(new_task)
+
+        print(f"บันทึก: {new_task}")
+        print(f"รายการทั้งหมด: {tasks}")
+        print("-"*30)
 
 if __name__ == "__main__":
     main()
